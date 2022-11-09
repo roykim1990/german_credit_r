010ac1b8fbd9c7d6db529672672eb301bbde44d6
merhi-odg <sami.merhi@opendatagroup.com>
2022-11-08 21:34:20 -0500

    match column names to data

--- german_credit.R
+++ german_credit.R
@@ -3,24 +3,24 @@ library(tidymodels)
 library(readr)
 
 # modelop.init
-begin <- function(){
+begin <- function() {
     # model <- read_rds("trained_model.rds")
     load("trained_model.RData")
     model <<- logreg_fit
-}   
+}
 
 # modelop.score
-action <- function(data){
-    df <- data.frame(data, stringsAsFactors=F)
+action <- function(data) {
+    df <- data.frame(data, stringsAsFactors = F)
     preds <- predict(model, df)
-    output <- list(label_value=df$label, score=preds$.pred_class)
+    output <- list(label_value = df$label, score = preds$.pred_class)
     emit(output)
 }
 
 # modelop.metrics
-metrics <- function(data){
+metrics <- function(data) {
     df <- data.frame(data)
     get_metrics <- metric_set(f_meas, accuracy, sensitivity, specificity, precision)
-    output <- get_metrics(data=df, truth=as.factor(X0.label_value), estimate=as.factor(X0.score))
+    output <- get_metrics(data = df, truth = as.factor(label_value), estimate = as.factor(score))
     emit(output)
 }
